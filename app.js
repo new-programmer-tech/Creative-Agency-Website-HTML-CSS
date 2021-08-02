0c7844970db21dc4bcac444598edf94a7afc2f22
harshal <harshalogale24@gmail.com>
2021-08-02 14:24:50 +0530

    js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 "use strict";
 
-let toggle=document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation')
+let toggle = document.querySelector(".toggle");
+let toggleClose = document.querySelector(".toggle-close");
+let navigation = document.querySelector(".navigation");
 // console.log(toggle);
 
-toggle.addEventListener('click', function()
-{   
-   navigation.classList.remove('navigation');
+toggle.addEventListener("click", function () 
+{
+  navigation.classList.add("navigation");
+  navigation.classList.remove("hide");
+  toggleClose.classList.remove("hide");
+  
 });
+
+
+toggleClose.addEventListener("click", function () 
+{
+  
+    toggleClose.classList.add("hide");
+    navigation.classList.remove("navigation");
+    navigation.classList.add("hide");
+  
+});
+
+
 
